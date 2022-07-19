17200807dca6eb84ccb221d65f80b4c9568bb9f7
lamhai1401 <lamthanhhai141@gmail.com>
2022-07-19 21:04:02 +0700

    add anonymous template pattern

--- template/template_pattern.go
+++ template/template_pattern.go
@@ -23,3 +23,38 @@ func (t *TemplateImpl) third() string {
 func (t *TemplateImpl) ExecuteAlgorithm(m MessageRetriever) string {
 	return strings.Join([]string{t.first(), m.Message(), t.third()}, " ")
 }
+
+type AnonymousTemplate struct{}
+
+func (a *AnonymousTemplate) first() string {
+	return "hello"
+}
+func (a *AnonymousTemplate) third() string {
+	return "template"
+}
+
+func (a *AnonymousTemplate) ExecuteAlgorithm(f func() string) string {
+	return strings.Join([]string{a.first(), f(), a.third()}, " ")
+}
+
+type TemplateAdapter struct {
+	myFunc func() string
+}
+
+func (a *TemplateAdapter) Message() string {
+	return ""
+}
+func MessageRetrieverAdapter(f func() string) MessageRetriever {
+	return &adapter{myFunc: f}
+}
+
+type adapter struct {
+	myFunc func() string
+}
+
+func (a *adapter) Message() string {
+	if a.myFunc != nil {
+		return a.myFunc()
+	}
+	return ""
+}
--- template/template_test.go
+++ template/template_test.go
@@ -25,4 +25,35 @@ func TestTemplate_ExecuteAlgorithm(t *testing.T) {
 				expected)
 		}
 	})
+
+	t.Run("Using anonymous functions", func(t *testing.T) {
+		m := new(AnonymousTemplate)
+		res := m.ExecuteAlgorithm(func() string {
+			return "world"
+		})
+		expectedOrError(res, " world ", t)
+	})
+
+	t.Run("Using anonymous functions adapted to an interface", func(t *testing.T) {
+		messageRetriever := MessageRetrieverAdapter(func() string {
+			return "world"
+		})
+
+		if messageRetriever == nil {
+			t.Fatal("Can not continue with a nil MessageRetriever")
+		}
+
+		template := &TestStruct{
+			Template: new(TemplateImpl),
+		}
+		res := template.ExecuteAlgorithm(messageRetriever)
+		expectedOrError(res, " world ", t)
+	})
+}
+
+func expectedOrError(res string, expected string, t *testing.T) {
+	if !strings.Contains(res, expected) {
+		t.Errorf("Expected string '%s' was not found on returned string\n",
+			expected)
+	}
 }
